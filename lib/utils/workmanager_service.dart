2fa6c7ad4633641ac60ec48eb3e2a53a7d7b9b9b
dwiki <yohanesdwiki08@Gmail.com>
2025-03-11 17:45:41 +0700

    update lib version

--- lib/utils/workmanager_service.dart
+++ lib/utils/workmanager_service.dart
@@ -45,7 +45,7 @@ class WorkmanagerService {
 
   Future<void> runPeriodicTask() async {
     final now = DateTime.now();
-    final targetTime = DateTime(now.year, now.month, now.day, 15, 41);
+    final targetTime = DateTime(now.year, now.month, now.day, 11, 00);
     final initialDelay = targetTime.isBefore(now)
         ? targetTime.add(const Duration(days: 1)).difference(now)
         : targetTime.difference(now);
